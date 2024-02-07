c888dbb84428ed6223f5a297ee62221f601e57a6
Tássyla Lima <56099401+tassyla@users.noreply.github.com>
2024-02-07 12:22:20 -0300

    Add files via upload

---
+++ exp5/desafio/circuito_exp5.v
@@ -0,0 +1,150 @@
+module circuito_exp5 (
+ input        clock,
+ input        reset,
+ input        iniciar,
+ input  [3:0] chaves,
+ output       acertou,
+ output       errou,
+ output       pronto,
+ output [3:0] leds,
+ output       db_timeout,
+ output       db_meio, 
+ output       db_igual,
+ output [6:0] db_contagem,
+ output [6:0] db_memoria,
+ output [6:0] db_estado,
+ output [6:0] db_jogadafeita,
+ output       db_clock,
+ output       db_iniciar,
+ output       db_tem_jogada  
+);
+
+wire fimC;
+wire contaC;
+wire zeraC;
+wire zeraR;
+wire contaT;
+wire zeraT;
+wire registraR;
+wire jogada_feita;
+wire igual;
+wire fimT;
+
+wire [3:0] estado_out;
+wire [3:0] contagem_out;
+wire [3:0] jogada_out;
+wire [3:0] memoria_out;
+
+// Unidade de controle ------------------------------
+exp5_unidade_controle unidade_controle(
+	.clock     (clock),
+	.reset     (reset),
+	.iniciar   (iniciar),
+	.fim       (fimC),
+    .jogada    (jogada_feita),
+	.igual     (igual), 
+    .fimT      (fimT),
+	.zeraC     (zeraC),
+	.contaC    (contaC),
+    .zeraT     (zeraT),
+    .contaT    (contaT),
+	.zeraR     (zeraR),
+	.registraR (registraR),
+    .timeout   (db_timeout),
+    .acertou   (acertou),
+    .errou     (errou),
+	.pronto    (pronto),
+	.db_estado (estado_out)
+); 
+
+// Fluxo de Dados ------------------------------
+exp5_fluxo_dados fluxo_dados (
+	.clock         (clock),
+	.zeraC         (zeraC),
+	.contaC        (contaC),
+    .zeraT         (zeraT),
+    .contaT        (contaT),
+	.zeraR         (zeraR),
+	.registraR     (registraR), 
+	.chaves        (chaves),
+	.igual         (igual),
+	.fimC          (fimC),
+    .fimT          (fimT),
+    .db_meio       (db_meio),
+    .jogada_feita  (jogada_feita),
+    .db_tem_jogada (db_tem_jogada),
+	.db_contagem   (contagem_out),
+	.db_memoria    (memoria_out),
+	.db_jogada     (jogada_out)
+);
+
+// Display0 -----------------------------------
+hexa7seg HEX0(
+	.hexa(contagem_out),
+	.display(db_contagem)
+);
+
+// Display1 -----------------------------------
+hexa7seg HEX1(
+	.hexa(memoria_out),
+	.display(db_memoria)
+);
+
+// Display2 -----------------------------------
+hexa7seg HEX2(
+	.hexa(jogada_out),
+	.display(db_jogadafeita)
+);
+
+// Display5 -----------------------------------
+hexa7seg HEX5(
+	.hexa(estado_out),
+	.display(db_estado)
+);
+
+assign db_igual = igual;
+assign db_clock = clock;
+assign db_iniciar = iniciar;
+
+assign leds = chaves;
+
+endmodule
+
+module hexa7seg (hexa, display);
+    input      [3:0] hexa;
+    output reg [6:0] display;
+
+    /*
+     *    ---
+     *   | 0 |
+     * 5 |   | 1
+     *   |   |
+     *    ---
+     *   | 6 |
+     * 4 |   | 2
+     *   |   |
+     *    ---
+     *     3
+     */
+        
+    always @(hexa)
+    case (hexa)
+        4'h0:    display = 7'b1000000;
+        4'h1:    display = 7'b1111001;
+        4'h2:    display = 7'b0100100;
+        4'h3:    display = 7'b0110000;
+        4'h4:    display = 7'b0011001;
+        4'h5:    display = 7'b0010010;
+        4'h6:    display = 7'b0000010;
+        4'h7:    display = 7'b1111000;
+        4'h8:    display = 7'b0000000;
+        4'h9:    display = 7'b0010000;
+        4'ha:    display = 7'b0001000;
+        4'hb:    display = 7'b0000011;
+        4'hc:    display = 7'b1000110;
+        4'hd:    display = 7'b0100001;
+        4'he:    display = 7'b0000110;
+        4'hf:    display = 7'b0001110;
+        default: display = 7'b1111111;
+    endcase
+endmodule
---
+++ exp5/desafio/exp5_fluxo_dados.v
@@ -0,0 +1,181 @@
+module exp5_fluxo_dados (
+input        clock,
+input        zeraC,
+input        contaC,
+input        zeraT,
+input        contaT,
+input        zeraR,
+input        registraR, 
+input [3:0]  chaves,
+output       igual,
+output       fimC,
+output       fimT,
+output       db_meio,
+output       jogada_feita,
+output       db_tem_jogada,
+output [3:0] db_contagem,
+output [3:0] db_memoria,
+output [3:0] db_jogada
+);
+
+wire [3:0] s_endereco;
+wire menor;
+wire maior;
+wire [3:0] s_dado;
+wire [3:0] s_chaves;
+wire sinal;
+
+  // contador_163
+  contador_163 contador (
+    .CLK( clock ),
+    .CLR( zeraC ),
+    .LD( 1'b1 ),
+    .ENP( contaC ),
+    .ENT( 1'b1 ),
+    .D( 4'b0 ),
+    .Q( s_endereco ),
+    .RCO( fimC )
+  );
+  
+  // comparador_85
+  comparador_85 comparador (
+    .A( s_dado ),
+    .B( s_chaves ),
+    .ALBi( 1'b0 ),
+    .AGBi( 1'b0 ),
+    .AEBi( 1'b1 ),
+    .ALBo( menor ),
+    .AGBo( maior ),
+    .AEBo( igual )
+  );
+  
+  // registrador
+  registrador_4 registrador(
+  .clock(clock),
+  .clear(zeraR),
+  .enable(registraR),
+  .D(chaves),
+  .Q(s_chaves)
+  );
+  
+  // memoria
+  sync_rom_16x4 memoria(
+  .clock(clock),
+  .address(s_endereco),
+  .data_out(s_dado)
+  );
+
+  // temporizador (contador_M)
+
+  contador_m temporizador(
+    .clock(clock),
+    .zera_as(zeraT),
+    .zera_s(zeraT),
+    .conta(contaT),
+    .Q(),
+    .fim(fimT),
+    .meio(db_meio)
+  );
+
+  // Geração do sinal das chaves
+  assign sinal = (chaves[0] ^ chaves[1] ^ chaves[2] ^ chaves[3]);
+  assign db_tem_jogada = sinal;
+
+  // edge detector
+    edge_detector edge_detector(
+    .clock(clock),
+    .reset(zeraC),
+    .sinal(sinal),
+    .pulso(jogada_feita)
+    );
+  
+assign db_contagem = s_endereco;
+assign db_jogada   = s_chaves;
+assign db_memoria  = s_dado;
+
+endmodule
+
+// REGISTRADOR SÍNCRONO DE 4 BITS -------------------------------------------------------
+module registrador_4 (
+    input        clock,
+    input        clear,
+    input        enable,
+    input  [3:0] D,
+    output [3:0] Q
+);
+
+    reg [3:0] IQ;
+
+    always @(posedge clock or posedge clear) begin
+        if (clear)
+            IQ <= 0;
+        else if (enable)
+            IQ <= D;
+    end
+
+    assign Q = IQ;
+
+endmodule
+
+// MEMÓRIA SÍNCRONA 16X4 -------------------------------------------------------
+module sync_rom_16x4 (clock, address, data_out);
+    input            clock;
+    input      [3:0] address;
+    output reg [3:0] data_out;
+
+    always @ (posedge clock)
+    begin
+        case (address)
+            4'b0000: data_out = 4'b0001;
+            4'b0001: data_out = 4'b0010;
+            4'b0010: data_out = 4'b0100;
+            4'b0011: data_out = 4'b1000;
+            4'b0100: data_out = 4'b0100;
+            4'b0101: data_out = 4'b0010;
+            4'b0110: data_out = 4'b0001;
+            4'b0111: data_out = 4'b0001;
+            4'b1000: data_out = 4'b0010;
+            4'b1001: data_out = 4'b0010;
+            4'b1010: data_out = 4'b0100;
+            4'b1011: data_out = 4'b0100;
+            4'b1100: data_out = 4'b1000;
+            4'b1101: data_out = 4'b1000;
+            4'b1110: data_out = 4'b0001;
+            4'b1111: data_out = 4'b0100;
+        endcase
+    end
+endmodule
+
+// CONTADOR -------------------------------------------------------
+module contador_163 ( CLK, CLR, LD, ENP, ENT, D, Q, RCO );
+    input CLK, CLR, LD, ENP, ENT;
+    input [3:0] D;
+    output reg [3:0] Q;
+    output reg RCO;
+
+    always @ (posedge CLK)  // Create the counter f-f behavior
+        if (CLR)               Q <= 4'd0;
+        else if (~LD)           Q <= D;
+        else if (ENT && ENP)    Q <= Q + 1;
+        else                    Q <= Q;
+ 
+    always @ (Q or ENT)     // Create RCO combinational output
+        if (ENT && (Q == 4'd15))   RCO = 1;
+        else                       RCO = 0;
+endmodule
+
+// COMPARADOR -------------------------------------------------------
+module comparador_85 (ALBi, AGBi, AEBi, A, B, ALBo, AGBo, AEBo);
+
+    input[3:0] A, B;
+    input      ALBi, AGBi, AEBi;
+    output     ALBo, AGBo, AEBo;
+    wire[4:0]  CSL, CSG;
+
+    assign CSL  = ~A + B + ALBi;
+    assign ALBo = ~CSL[4];
+    assign CSG  = A + ~B + AGBi;
+    assign AGBo = ~CSG[4];
+    assign AEBo = ((A == B) && AEBi);
+
+endmodule /* comparador_85 */
