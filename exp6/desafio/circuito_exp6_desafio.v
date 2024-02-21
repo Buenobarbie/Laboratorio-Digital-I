581e37adc7af92794062f38f2358f5954f152764
Buenobarbie <barbarabueno140104@gmail.com>
2024-02-21 12:05:20 -0300

    Código desafio

---
+++ exp6/desafio/circuito_exp6_desafio.v
@@ -0,0 +1,141 @@
+module circuito_exp6 (
+ input        clock,
+ input        reset,
+ input        iniciar,
+ input  [3:0] botoes,
+ output [3:0] leds,
+ output       pronto,
+ output       ganhou,
+ output       perdeu,
+ output       db_clock,
+ output       db_tem_jogada  ,
+ output       db_igual,
+ output       db_enderecoIgualRodada,
+ output       db_timeout,
+ output [6:0] db_contagem,
+ output [6:0] db_memoria,
+ output [6:0] db_jogadafeita,
+ output [6:0] db_rodada,
+ output [6:0] db_estado
+);
+
+wire fimE;
+wire contaE;
+wire zeraE;
+
+wire zeraR;
+wire registraR;
+
+wire contaT;
+wire zeraT;
+wire fimT;
+
+wire we;
+
+wire contaRod;
+wire zeraRod;
+wire fimRod;
+
+wire jogada_feita;
+wire igual;
+wire enderecoIgualRodada;
+
+wire [3:0] estado_out;
+wire [3:0] contagem_out;
+wire [3:0] jogada_out;
+wire [3:0] memoria_out;
+wire [3:0] rodada_out;
+
+// Unidade de controle ------------------------------
+unidade_controle_exp6 unidade_controle(
+	.clock     (clock),
+	.reset     (reset),
+	.iniciar   (iniciar),
+	.fimE      (fimE),
+	.fimRod    (fimRod),
+	.fimT      (fimT),
+    .jogada    (jogada_feita),
+	.igual     (igual), 
+	.enderecoIgualRodada (enderecoIgualRodada),
+	.zeraE     (zeraE),
+	.contaE    (contaE),
+	.zeraRod   (zeraRod),
+	.contaRod  (contaRod),
+	.zeraT     (zeraT),
+	.contaT    (contaT),
+	.zeraR     (zeraR),
+	.registraR (registraR),
+	.we        (we),
+	.acertou   (ganhou),
+	.errou     (perdeu),
+    .timeout   (db_timeout),
+	.pronto    (pronto),
+	.db_estado (estado_out)
+); 
+
+// Fluxo de Dados ------------------------------
+fluxo_dados_exp6 fluxo_dados (
+	.clock         (clock),
+	.zeraE         (zeraE),
+	.contaE        (contaE),
+	.contaRod      (contaRod),
+	.zeraRod       (zeraRod),
+    .zeraT         (zeraT),
+    .contaT        (contaT),
+	.zeraR         (zeraR),
+	.registraR     (registraR), 
+	.chaves        (botoes),
+	.we            (we),
+	.fimE          (fimE),
+	.fimRod        (fimRod),
+    .fimT          (fimT),
+	.igual         (igual),
+	.enderecoIgualRodada (enderecoIgualRodada),
+    .jogada_feita  (jogada_feita),
+    .db_tem_jogada (db_tem_jogada),
+	.db_contagem   (contagem_out),
+	.db_memoria    (memoria_out),
+	.db_jogada     (jogada_out),
+	.db_rodada	   (rodada_out)
+);
+
+// Display0 -----------------------------------
+hexa7seg HEX0(
+	.hexa(contagem_out),
+	.display(db_contagem)
+);
+
+// Display1 -----------------------------------
+hexa7seg HEX1(
+	.hexa(memoria_out),
+	.display(db_memoria)
+);
+
+// Display2 -----------------------------------
+hexa7seg HEX2(
+	.hexa(jogada_out),
+	.display(db_jogadafeita)
+);
+
+// Display3 -----------------------------------
+hexa7seg HEX3(
+	.hexa(rodada_out),
+	.display(db_rodada)
+);
+
+// Display5 -----------------------------------
+hexa7seg HEX5(
+	.hexa(estado_out),
+	.display(db_estado)
+);
+
+assign db_enderecoIgualRodada = enderecoIgualRodada;
+
+assign db_igual = igual;
+assign db_clock = clock;
+
+// LEDs indicam a posição lida na memória
+assign leds = memoria_out;
+
+endmodule
+
---
+++ exp6/desafio/fluxo_dados_exp6_desafio.v
@@ -0,0 +1,127 @@
+module fluxo_dados_exp6 (
+input        clock,
+input        zeraE,
+input        contaE,
+input        contaRod,
+input        zeraRod,
+input        zeraT,
+input        contaT,
+input        zeraR,
+input        registraR, 
+input [3:0]  chaves,
+input        we,
+output       fimE,
+output       fimRod,
+output       fimT,
+output       igual,
+output       enderecoIgualRodada,
+output       jogada_feita,
+output       db_tem_jogada,
+output [3:0] db_contagem,
+output [3:0] db_memoria,
+output [3:0] db_jogada,
+output [3:0] db_rodada
+);
+
+wire [3:0] s_endereco;
+wire [3:0] s_rodada;
+wire [3:0] s_dado;
+wire [3:0] s_chaves;
+wire sinal;
+
+  // contador do Endereco
+  contador_163 ContEnd (
+    .CLK( clock ),
+    .CLR( zeraE ),
+    .LD( 1'b1 ),
+    .ENP( contaE ),
+    .ENT( 1'b1 ),
+    .D( 4'b0 ),
+    .Q( s_endereco ),
+    .RCO( fimE )
+  );
+
+  // contador da Rodada
+  contador_163 ContRod (
+    .CLK( clock ),
+    .CLR( zeraRod ),
+    .LD( 1'b1 ),
+    .ENP( contaRod ),
+    .ENT( 1'b1 ),
+    .D( 4'b0 ),
+    .Q(s_rodada),
+    .RCO( fimRod )
+  );
+  
+  // Compara Rodada
+  comparador_85 comparadorR (
+    .A( s_rodada ),
+    .B( s_endereco ),
+    .ALBi( 1'b0 ),
+    .AGBi( 1'b0 ),
+    .AEBi( 1'b1 ),
+    .ALBo(  ),
+    .AGBo(  ),
+    .AEBo( enderecoIgualRodada )
+  );
+
+  // Compara Jogada
+  comparador_85 comparadorJ (
+    .A( s_dado ),
+    .B( s_chaves ),
+    .ALBi( 1'b0 ),
+    .AGBi( 1'b0 ),
+    .AEBi( 1'b1 ),
+    .ALBo(  ),
+    .AGBo(  ),
+    .AEBo( igual )
+  );
+  
+  // registrador
+  registrador_4 registrador(
+  .clock(clock),
+  .clear(zeraR),
+  .enable(registraR),
+  .D(chaves),
+  .Q(s_chaves)
+  );
+  
+  // memoria
+  sync_ram_16x4_file memoria(
+  .clk(clock),
+  .we(we),
+  .data(s_chaves),
+  .addr(s_endereco),
+  .q(s_dado)
+  );
+
+  // temporizador (contador_M)
+
+  contador_m #(3000, 12)temporizador(
+    .clock(clock),
+    .zera_as(zeraT),
+    .zera_s(zeraT),
+    .conta(contaT),
+    .Q(),
+    .fim(fimT),
+    .meio()
+  );
+
+  // Geração do sinal das chaves
+  assign sinal = (chaves[0] ^ chaves[1] ^ chaves[2] ^ chaves[3]);
+  assign db_tem_jogada = sinal;
+
+  // edge detector
+    edge_detector edge_detector(
+    .clock(clock),
+    .reset(zeraRod),
+    .sinal(sinal),
+    .pulso(jogada_feita)
+    );
+  
+assign db_contagem = s_endereco;
+assign db_jogada   = s_chaves;
+assign db_memoria  = s_dado;
+assign db_rodada   = s_rodada;
+
+endmodule
