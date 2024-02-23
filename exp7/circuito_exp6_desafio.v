d7a3905f6dcd12f7bed0d447847118fff9500b19
Tássyla Lima <56099401+tassyla@users.noreply.github.com>
2024-02-23 17:12:43 -0300

    exp 7; co-authored: babs e juliana

---
+++ exp7/circuito_exp6_desafio.v
@@ -0,0 +1,155 @@
+module circuito_exp6 (
+ input        clock,
+ input        reset,
+ input        iniciar,
+ input  [3:0] botoes,
+ output [3:0] leds,
+ output       pronto,
+ output       ganhou,
+ output       perdeu,
+ output       db_clock,
+ output       db_tem_jogada  ,
+ output       db_igual,
+ output       db_enderecoIgualRodada,
+ output       db_timeout,
+ output [6:0] db_contagem,
+ output [6:0] db_memoria,
+ output [6:0] db_jogadafeita,
+ output [6:0] db_rodada,
+ output [6:0] db_estado
+);
+
+wire fimE;
+wire contaE;
+wire zeraE;
+
+wire zeraR;
+wire registraR;
+
+wire contaT;
+wire zeraT;
+wire fimT;
+
+wire contaP;
+wire zeraP;
+wire fimP;
+
+wire we;
+
+wire sinal_led;
+
+wire contaRod;
+wire zeraRod;
+wire fimRod;
+
+wire jogada_feita;
+wire igual;
+wire enderecoIgualRodada;
+
+wire [3:0] estado_out;
+wire [3:0] contagem_out;
+wire [3:0] jogada_out;
+wire [3:0] memoria_out;
+wire [3:0] rodada_out;
+
+// Unidade de controle ------------------------------
+unidade_controle_exp6 unidade_controle(
+	.clock     (clock),
+	.reset     (reset),
+	.iniciar   (iniciar),
+	.fimE      (fimE),
+	.fimRod    (fimRod),
+	.fimT      (fimT),
+	.fimP      (fimP),
+    .jogada    (jogada_feita),
+	.igual     (igual), 
+	.enderecoIgualRodada (enderecoIgualRodada),
+	.zeraE     (zeraE),
+	.contaE    (contaE),
+	.contaP    (contaP),
+	.zeraRod   (zeraRod),
+	.contaRod  (contaRod),
+	.zeraT     (zeraT),
+	.contaT    (contaT),
+	.zeraR     (zeraR),
+	.registraR (registraR),
+	.we        (we),
+	.acertou   (ganhou),
+	.errou     (perdeu),
+    .timeout   (db_timeout),
+	.pronto    (pronto),
+	.db_estado (estado_out),
+	.sinal_led (sinal_led), 
+); 
+
+// Fluxo de Dados ------------------------------
+fluxo_dados_exp6 fluxo_dados (
+	.clock         (clock),
+	.zeraE         (zeraE),
+	.contaE        (contaE),
+	.contaRod      (contaRod),
+	.zeraRod       (zeraRod),
+    .zeraT         (zeraT),
+	.zeraP         (zeraP),
+    .contaT        (contaT),
+	.contaP        (contaP),
+	.zeraR         (zeraR),
+	.registraR     (registraR), 
+	.chaves        (botoes),
+	.we            (we),
+	.sinal_led     (sinal_led),
+	.fimE          (fimE),
+	.fimRod        (fimRod),
+    .fimT          (fimT),
+	.fimP          (fimP),
+	.igual         (igual),
+	.enderecoIgualRodada (enderecoIgualRodada),
+    .jogada_feita  (jogada_feita),
+    .db_tem_jogada (db_tem_jogada),
+	.db_contagem   (contagem_out),
+	.db_memoria    (memoria_out),
+	.db_jogada     (jogada_out),
+	.db_rodada	   (rodada_out),
+	.leds          (leds)
+);
+
+// Display0 -----------------------------------
+hexa7seg HEX0(
+	.hexa(contagem_out),
+	.display(db_contagem)
+);
+
+// Display1 -----------------------------------
+hexa7seg HEX1(
+	.hexa(memoria_out),
+	.display(db_memoria)
+);
+
+// Display2 -----------------------------------
+hexa7seg HEX2(
+	.hexa(jogada_out),
+	.display(db_jogadafeita)
+);
+
+// Display3 -----------------------------------
+hexa7seg HEX3(
+	.hexa(rodada_out),
+	.display(db_rodada)
+);
+
+// Display5 -----------------------------------
+hexa7seg HEX5(
+	.hexa(estado_out),
+	.display(db_estado)
+);
+
+assign db_enderecoIgualRodada = enderecoIgualRodada;
+
+assign db_igual = igual;
+assign db_clock = clock;
+
+
+
+
+endmodule
+
---
+++ exp7/unidade_controle_desafio.v
@@ -0,0 +1,126 @@
+
+module unidade_controle_exp6 (
+    input      clock,
+    input      reset,
+    input      iniciar,
+    input      fimE,
+    input      fimRod,
+    input      fimT,
+    input      fimP,
+    input      jogada,
+    input      igual,
+    input      enderecoIgualRodada,
+    output reg zeraE,
+    output reg contaE,
+    output reg contaP,
+    output reg zeraRod,
+    output reg contaRod,
+    output reg zeraT,
+    output reg zeraP,
+    output reg contaT,
+    output reg zeraR,
+    output reg registraR,
+    output reg we,
+    output reg acertou,
+    output reg errou,
+    output reg timeout,
+    output reg pronto,
+    output reg [3:0] db_estado,
+    output reg sinal_led
+);
+
+    // Define estados
+    parameter inicial              = 4'b0000;  // 0
+    parameter preparacao           = 4'b0001;  // 1
+    parameter exibe_jogada_inicial = 4'b1111;  // F
+    parameter inicia_rodada        = 4'b0010;  // 2
+    parameter espera_jogada        = 4'b0011;  // 3
+    parameter registra             = 4'b0100;  // 4
+    parameter comparacao           = 4'b0101;  // 5
+    parameter proximo              = 4'b0110;  // 6
+    parameter ultima_rodada        = 4'b0111;  // 7 
+    parameter proxima_rodada       = 4'b1000;  // 8
+    parameter espera_nova_jogada   = 4'b1001;  // 9
+    parameter registra_nova_jogada = 4'b1011;  // B
+    parameter escreve_memoria      = 4'b1101;  // D
+    parameter fim_errou            = 4'b1110;  // E
+    parameter fim_acertou          = 4'b1010;  // A  
+    parameter fim_timeout          = 4'b1100;  // C
+
+    // Variaveis de estado
+    reg [3:0] Eatual, Eprox;
+
+    // Memoria de estado
+    always @(posedge clock or posedge reset) begin
+        if (reset)
+            Eatual <= inicial;
+        else
+            Eatual <= Eprox;
+    end
+        
+    // Logica de proximo estado 
+    always @* begin
+        case (Eatual) 
+            inicial:              Eprox = (iniciar) ? preparacao : inicial;
+            preparacao:           Eprox = inicia_rodada; 
+            inicia_rodada:        Eprox = espera_jogada;
+            exibe_jogada_inicial: Eprox = (fimP) ? espera_jogada : exibe_jogada_inicial;
+            espera_jogada:        Eprox = (jogada) ? registra : (fimT) ? fim_timeout : espera_jogada;
+            registra:             Eprox = comparacao;
+            comparacao:           Eprox = (~igual) ? fim_errou : (enderecoIgualRodada) ? ultima_rodada : proximo;
+            proximo:              Eprox = espera_jogada;
+            ultima_rodada:        Eprox = (fimRod) ? fim_acertou : espera_nova_jogada;
+            espera_nova_jogada:   Eprox = (jogada) ? registra_nova_jogada : (fimT) ? fim_timeout : espera_nova_jogada;
+            registra_nova_jogada: Eprox = escreve_memoria; 
+            escreve_memoria:      Eprox = proxima_rodada;
+            proxima_rodada:       Eprox = inicia_rodada; 
+            fim_errou:            Eprox = (iniciar) ? preparacao : fim_errou;
+            fim_acertou:          Eprox = (iniciar) ? preparacao : fim_acertou;
+            fim_timeout:          Eprox = (iniciar) ? preparacao : fim_timeout; 
+            default:              Eprox = inicial;
+        endcase
+    end
+
+    // Logica de saida (maquina Moore)
+    always @* begin
+        zeraE     = (Eatual == inicial || Eatual == preparacao || Eatual == inicia_rodada) ? 1'b1 : 1'b0;
+        zeraR     = (Eatual == inicial || Eatual == preparacao) ? 1'b1 : 1'b0;
+        zeraP     = (Eatual == inicial || Eatual == preparacao) ? 1'b1 : 1'b0;
+        zeraRod   = (Eatual == inicial || Eatual == preparacao) ? 1'b1 : 1'b0;
+        zeraT     = (Eatual == inicial || Eatual == preparacao || Eatual== proximo || Eatual == ultima_rodada) ? 1'b1 : 1'b0;
+        registraR = (Eatual == registra || Eatual == registra_nova_jogada) ? 1'b1 : 1'b0;
+        contaE    = (Eatual == proximo || Eatual == ultima_rodada) ? 1'b1 : 1'b0;
+        contaT    = (Eatual == espera_jogada || Eatual == espera_nova_jogada) ? 1'b1 : 1'b0;
+        contaP    = (Eatual == exibe_jogada_inicial) ? 1'b1 : 1'b0;
+        contaRod  = (Eatual == proxima_rodada) ? 1'b1 : 1'b0;
+        pronto    = (Eatual == fim_acertou || Eatual == fim_errou || Eatual == fim_timeout) ? 1'b1 : 1'b0;
+        acertou   = (Eatual == fim_acertou) ? 1'b1 : 1'b0;
+        errou     = (Eatual == fim_errou) ? 1'b1 : 1'b0;
+        timeout   = (Eatual == fim_timeout) ? 1'b1 : 1'b0; 
+        we        = (Eatual == escreve_memoria) ? 1'b1 : 1'b0;
+        sinal_led = (Eatual == exibe_jogada_inicial) ? 1'b1 : 1'b0; 
+
+        // Saida de depuracao (estado)
+        case (Eatual)
+            inicial:              db_estado = inicial;         // 0
+            preparacao:           db_estado = preparacao;      // 1
+            exibe_jogada_inicial: db_estado = exibe_jogada_inicial; // F
+            inicia_rodada:        db_estado = inicia_rodada;   // 2
+            espera_jogada:        db_estado = espera_jogada;   // 3
+            registra:             db_estado = registra;        // 4
+            comparacao:           db_estado = comparacao;      // 5
+            proximo:              db_estado = proximo;         // 6
+            ultima_rodada:        db_estado = ultima_rodada;   // 7
+            proxima_rodada:       db_estado = proxima_rodada;  // 8
+            espera_nova_jogada:   db_estado = espera_nova_jogada; // 9
+            registra_nova_jogada: db_estado = registra_nova_jogada; // B
+            escreve_memoria:      db_estado = escreve_memoria; // D
+            fim_errou:            db_estado = fim_errou;       // E
+            fim_acertou:          db_estado = fim_acertou;     // A
+            fim_timeout:          db_estado = fim_timeout;     // C
+            
+            default:       db_estado = 4'b1111;           // F
+        endcase
+    end
+
+endmodule
